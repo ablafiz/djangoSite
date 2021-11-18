f84469e8a73d8e8750442db5e70e745c828496d1
ablafiz <ablafizi@gmail.com>
2021-11-18 12:04:52 +0300

    the message

---
+++ house/models.py
@@ -0,0 +1,19 @@
+from typing import ChainMap
+from django.db import models
+from django.conf import settings
+from django.db.models.expressions import Window
+from django.db.models.fields import CharField
+from django.utils import timezone
+
+class House(models.Model):
+    region = models.CharField(max_length=20)
+    district = models.CharField(max_length=20)
+    ward = models.CharField(max_length=20)
+    vilage = models.CharField(max_length=20)
+    strict = models.CharField(max_length=20)
+
+    def saveme(self):
+        self.save()
+    
+    def __str__(self):
+        return "The house is located at "+ self.region+" Region"
--- post/models.py
+++ post/models.py
@@ -31,10 +31,10 @@ class Student(models.Model):
     firstname = models.CharField(max_length=20)
     middlename = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20)
-    createdon = models.DateTimeField(default= "2021-10-01")
+    createdon = models.DateTimeField(default=timezone.now)
 
     def publish(self):
         self.save()
     
     def __str__(self):
-        return self.firstname +" "+self.middlename +" "+self.lastname
+        return self.firstname +" "+ self.middlename +" "+ self.lastname
