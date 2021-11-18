cd8b72f9f0e1d2d8a2123b2bbe08e923ace56391
ablafizi@gmail.com <ablafizi@gmail.com>
2021-11-18 09:23:40 +0300

    Initial commit

---
+++ post/models.py
@@ -0,0 +1,28 @@
+from typing import ChainMap
+from django.db import models
+from django.conf import settings
+from django.db.models.fields import CharField
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    other_field1= models.CharField(max_length=20) 
+    other_field2= models.CharField(max_length=20) 
+    other_field3= models.CharField(max_length=20) 
+    other_field4= models.CharField(max_length=20) 
+    other_field5= models.CharField(max_length=20) 
+    other_field6= models.CharField(max_length=20) 
+    other_field7= models.CharField(max_length=20) 
+
+    def todb(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.text+ " " + str(self.created_date) +""+str(self.published_date)
